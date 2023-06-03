e9af28e3ead90d9f7e457934a68dc2b931c6f8c7
Leonardo Ledda <leonardoledda@gmail.com>
2023-06-03 16:14:12 +0200

    PixelPropsUtils: Update fingerprints
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- core/java/com/android/internal/util/fluid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/fluid/PixelPropsUtils.java
@@ -67,7 +67,7 @@ public class PixelPropsUtils {
         propsToChange.put("DEVICE", "redfin");
         propsToChange.put("PRODUCT", "redfin");
         propsToChange.put("MODEL", "Pixel 5");
-        propsToChange.put("FINGERPRINT", "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys");
+        propsToChange.put("FINGERPRINT", "google/redfin/redfin:13/TQ2A.230505.002/9891397:user/release-keys");
 
         propsToChangePixel3XL = new HashMap<>();
         propsToChangePixel3XL.put("BRAND", "google");
@@ -75,7 +75,7 @@ public class PixelPropsUtils {
         propsToChangePixel3XL.put("DEVICE", "crosshatch");
         propsToChangePixel3XL.put("PRODUCT", "crosshatch");
         propsToChangePixel3XL.put("MODEL", "Pixel 3 XL");
-        propsToChangePixel3XL.put("FINGERPRINT", "google/crosshatch/crosshatch:11/RQ3A.210705.001/7380771:user/release-keys");
+        propsToChangePixel3XL.put("FINGERPRINT", "google/crosshatch/crosshatch:12/SP1A.210812.016.C2/8618562:user/release-keys");
     }
 
     public static void setProps(String packageName) {
